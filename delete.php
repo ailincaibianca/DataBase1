5389751623f3745a59c3d5f395b3dcd6188125b0
ailincaibianca <63391401+ailincaibianca@users.noreply.github.com>
2020-04-30 12:41:30 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,7 +11,7 @@ $stmt1=$con->prepare($sql1);
 $stmt2=$con->prepare($sql2);
 $stmt1->execute();
 $stmt2->execute();
-$sqltrigger2="CREATE TRIGGER ADTrigger AFTER DELETE ON authors FOR EACH ROW
+$sqltrigger2="CREATE TRIGGER ADTrigger AFTER DELETE ON products FOR EACH ROW
         BEGIN
         INSERT INTO products_updated (ID, Name, Status,EdTime) 
         VALUES (NULL, OLD.Name, 'DELETED',NOW());
