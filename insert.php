07948864d70a4308c9fefea669d60f0f53047def
ailincaibianca <63391401+ailincaibianca@users.noreply.github.com>
2020-04-30 08:06:20 +0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,7 +17,7 @@
                         <input type="file" name="myimage" placeholder="Your Image">
                     </li>
                     <li>
-                        <input class="submitbtn" type="submit" value="Add new author to database" name='add'>
+                        <input class="submitbtn" type="submit" value="Add new product to database" name='add'>
                     </li>
                 </ul>
             </form>
